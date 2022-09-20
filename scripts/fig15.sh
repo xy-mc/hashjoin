811adc6dd6e4dbae28b6112758a332ac41e403ee
Vikram Narayanan <vikram186@gmail.com>
2022-09-20 00:05:54 -0400

    Fix fig15.sh script

--- scripts/fig15.sh
+++ scripts/fig15.sh
@@ -2,7 +2,11 @@
 
 set -euo pipefail
 
-output="fig15-$(date -Iseconds).csv"
+output_prefix="fig15-$(date -Iseconds)"
+output="${output_prefix}.csv"
+output_dat="${output_prefix}.dat"
+
+NUM_CYCLES_PER_SEC=$(($(lscpu | grep "CPU MHz:" | awk '{print $NF}' | cut -d'.' -f1) * 1000000))
 
 function finish {
   ret=$?
@@ -13,12 +17,42 @@ function finish {
 }
 trap finish EXIT
 
->&2 echo "Output CSV: $output"
-echo "passes, radix bits, runtime total, build, part, total time, total tuples, cycles per tuple" >$output
 
-#for million in 128 384 640 896 1152 1408 1664 1920; do
-for million in 1152 1408 1664 1920; do
+echo "Building for 128, 256, and 384 Million tuples with NUM_PASSES=1, NUM_RADIX_BITS=12 ..."
+
+make distclean &> build.log
+./configure --disable-key8B CPPFLAGS="-DNUM_PASSES=1 -DNUM_RADIX_BITS=12" &>> build.log
+make &> build.log
+
+echo "Running with R=S=(128 256 384) million tuples with NUM_PASSES=1, NUM_RADIX_BITS=12 ..."
+>&2 echo "Output CSV: $output"
+echo "passes, radix bits, runtime total, build, part, total time (usecs), total tuples, cycles per tuple" >$output
+
+for million in 128 256 384 ; do
   tuples=$((million * 1000000))
   >&2 echo "--- Running #$tuples"
-  ./src/mchashjoins -n 64 -r "$tuples" -s "$tuples" | tee -a "$output"
+  ./src/mchashjoins -n 64 -r "$tuples" -s "$tuples" --basic-numa | tee -a "$output"
+done
+
+echo "Building for tuples > 1Billion with NUM_PASSES=2, NUM_RADIX_BITS=18 ..."
+
+make distclean &> build.log
+./configure --disable-key8B CPPFLAGS="-DNUM_PASSES=2 -DNUM_RADIX_BITS=18" &> build.log
+make &> build.log
+
+echo "Running with R=S=(512 768 1024 1280 1408 1536 1792) million tuples with NUM_PASSES=2, NUM_RADIX_BITS=18 ..."
+
+for million in 512 768 1024 1280 1408 1536 1792 ; do
+  tuples=$((million * 1000000))
+  >&2 echo "--- Running #$tuples"
+  ./src/mchashjoins -n 64 -r "$tuples" -s "$tuples" --basic-numa | tee -a "$output"
+done
+
+echo "num_tuples (mill), tuples_per_sec (mill)" > $output_dat
+tail -n +2 ${output} | while read line; do
+  CYCLES_PER_TUPLE=$(echo ${line} | awk -F',' '{ print $NF }')
+  TOTAL_TUPLES=$(echo ${line} | awk -F',' '{ print $(NF-1) }')
+  TUPLES_PER_SEC=$(echo "${NUM_CYCLES_PER_SEC} / ${CYCLES_PER_TUPLE} / 1000000" | bc)
+  NUM_TUPLES_MIL=$(echo "${TOTAL_TUPLES} / 1000000" | bc)
+  echo ${NUM_TUPLES_MIL}, ${TUPLES_PER_SEC} | tee -a "$output_dat"
 done
